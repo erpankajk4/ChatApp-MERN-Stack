01359e43627789690fa0beaa9ff3c7e13538c7ad
pankaj <er.pankaj,k4@gmail.com>
2024-04-16 22:56:19 +0530

    d2

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:8000/", {
+			const socket = io("https://chatapp-mern-stack.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
